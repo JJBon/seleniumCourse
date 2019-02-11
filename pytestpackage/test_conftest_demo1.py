e9fdbac287fd83a2d3b9884269bf79bbc59d65cd
Juan Jose Bonilla <jjbonillatroncoso@gmail.com>
2019-02-11 12:49:30 -0500

    pytest practice

---
+++ pytestpackage/test_conftest_demo1.py
@@ -0,0 +1,6 @@
+
+def test_demo1_methodA(oneTimeSetUp, setUp):
+    print("Running demo1 method A")
+
+def test_demo1_methodB(oneTimeSetUp,setUp):
+    print("Running demo1 method B")
---
+++ pytestpackage/test_conftest_demo2.py
@@ -0,0 +1,6 @@
+
+def test_demo2_methodA(oneTimeSetUp, setUp):
+    print("Running demo2 method A")
+
+def test_demo2_methodB(oneTimeSetUp, setUp):
+    print("Running demo2 method B")
